25a2bea7e6dec8e58355b24710ea5d6f64724ef9
Laura Ortiz <a2024111203@estudiantes.upsa.edu.bo>
2025-09-15 01:35:57 -0400

    -new2

--- UPSA203API/Program.cs
+++ UPSA203API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipelinejehjeheheh. 
-//CRUD1-new
+//CRUD1-new-new2
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
